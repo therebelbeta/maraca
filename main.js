99bdcf8e65d8dc45427eb3479f6ccd299dda6c7f
Trent Oswald <trentoswald@therebelrobot.com>
2014-03-27 14:16:32 -0600

    one more time

--- main.js
+++ main.js
@@ -7,7 +7,32 @@ var app = new Vue({
 })
 var canVibrate = "vibrate" in navigator || "mozVibrate" in navigator;
 app.vibrateEnabled = canVibrate;
-if (canVibrate && !("vibrate" in navigator)){
-  navigator.vibrate = navigator.mozVibrate;
-}
-navigator.vibrate([10000,10000,10000,10000,10000,10000,10000,10000,10000,10000]);
+
+var patterns = {
+  'continuous': 10000,
+};
+
+var pattern, v;
+
+navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;
+
+var setupVibe = function() {
+  if (v) {
+    clearInterval(v);
+    v = null;
+  }
+
+  pattern = 'continuous';
+  navigator.vibrate(patterns[pattern]);
+  if (pattern == 'continuous') {
+    v = setInterval(vibe, patterns[pattern]);
+  } else {
+    d = patterns[pattern].reduce(function(a,b){return a+b});
+    v = setInterval(vibe, d);
+  }
+};
+
+var vibe = function() {
+  navigator.vibrate(patterns[pattern]);
+};
+setupVibe();
